f7ed61ae757ede3bbd5363cadec0e91e9ccd7359
Kamwele-hub <stephen.kamwele@student.moringaschool.com>
2024-04-28 19:51:25 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+const companyName = "Scuber"
+let mostProfitableNeighborhood = "Chelsea"
+function getMostProfitableNeighbourhood (){
+    mostProfitableNeighborhood = "Chelsea"
+}
+
+let companyCeo = 'Susan Smith'
+function getCompanyCeo () {
+    companyCeo = "Susan Smith"
+}
